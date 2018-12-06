214898b49d912607f92ba056b09945691b80e8ad
Wolf McNally <wolf@wolfmcnally.com>
2018-12-05 18:02:36 -0800

    Stopped using obsoleted GeneralError.

--- WolfApp/Classes/Shared/WolfAppError.swift
+++ WolfApp/Classes/Shared/WolfAppError.swift
@@ -1,8 +1,8 @@
 //
-//  BundleExtensions.swift
+//  WolfAppError.swift
 //  WolfApp
 //
-//  Created by Wolf McNally on 6/23/17.
+//  Created by Wolf McNally on 12/5/18.
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
@@ -22,30 +22,12 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //  SOFTWARE.
 
-import Foundation
 import WolfFoundation
 
-/// WolfApp.BundleClass.self can be used as an argument to the Bundle.findBundle(forClass:) method to search within this framework bundle.
-public class BundleClass { }
+public struct WolfAppError: MessageError {
+    public let message: String
 
-extension Bundle {
-    /// Similar to Bundle.bundleForClass, except if aClass is nil (or omitted) the main bundle is returned
-    public static func findBundle(forClass aClass: AnyClass? = nil) -> Bundle {
-        let bundle: Bundle
-        if let aClass = aClass {
-            bundle = Bundle(for: aClass)
-        } else {
-            bundle = Bundle.main
-        }
-        return bundle
-    }
-
-    public static func urlForResource(_ name: String, withExtension anExtension: String? = nil, subdirectory subpath: String? = nil) -> (Bundle) throws -> URL {
-        return { bundle in
-            guard let url = bundle.url(forResource: name, withExtension: anExtension, subdirectory: subpath) else {
-                throw WolfAppError("Resource not found.")
-            }
-            return url
-        }
+    public init(_ message: String) {
+        self.message = message
     }
 }
